7b1b8f1e646ed402a26681fba11f6bb6f94df558
Sidi Yaya Konaté <konateze1@gmail.com>
2022-09-07 14:12:46 -0400

    Building a basic calculator.
    Addition

---
+++ basic c++ /blinding a calculator.cpp
@@ -0,0 +1,28 @@
+//
+//  blinding a calculator.cpp
+//  basic c++ 2
+//
+//  Created by Sidi Yaya Konaté on 2022-09-07.
+//
+
+// calculator
+
+#include <iostream>>
+using namespace std;
+
+int main(){
+    
+    int number1, number2, sum;
+    
+    cout << "Enter first number:  ";
+    cin>> number1;
+    cout << "Enter second number:  ";
+    cin>> number2;
+    
+    sum = number1 + number2;
+    
+    cout << sum<<endl;
+    
+    
+    return 0;
+}
