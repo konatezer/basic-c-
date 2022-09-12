ca50ebc02a4e5cfd61dbd6fea40b5424efb925b5
Sidi Yaya Konaté <konateze1@gmail.com>
2022-09-12 09:45:05 -0400

    Arrays
    The Arrays

---
+++ basic c++ /Arrays.cpp
@@ -0,0 +1,20 @@
+//
+//  Arrays.cpp
+//  basic c++ 2
+//
+//  Created by Sidi Yaya Konaté on 2022-09-07.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int luckyNums[] = {4, 8 , 18, 5, 2, 38} ;
+    
+    
+    cout<< luckyNums[0] <<endl;
+    
+    
+    return 0;
+}
